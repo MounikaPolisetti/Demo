1fce4a81b01f69a25b4da762747c6d3a822c819b
Mounika <mounika25.p@gmail.com>
2017-10-12 14:11:46 -0700

    Hi

--- Demo/src/Demo1.java
+++ Demo/src/Demo1.java
@@ -2,7 +2,13 @@ public class Demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome");
+		int a = 10;
+		int b = 20;
+		System.out.println(a);
+		System.out.println(b);
 		System.out.println("Bye");
+		
+		
 	}
 
 }
